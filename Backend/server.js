6b5ec00625e006ca61d153115c879ed2129b89db
Gowthamkumarks17 <gowthmks1010@gmail.com>
2026-03-27 15:16:47 +0530

    corrected the error in server.js to get correct config folder

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('./Config/db');
+const connectDB = require('./config/db');
 const routes = require('./Routes/routes');
 
 const app = express();
